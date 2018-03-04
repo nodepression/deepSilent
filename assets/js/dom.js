ba87282e3e01f6c99bee778b68102b1ccf83d0a8
nodepression <qianzgcn@163.com>
2018-03-04 21:51:09 +0800

    第二页调整

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -15,22 +15,41 @@ const dom0 = ` <div id="dowebok">
 
 
 const dom1=` <div id="dowebok">
-                <div class="section">
-                <h2 class="side-title">摘要</h2>
-                <h2 class="content-title">摘要</h2>
-                <div class="long-words-box" id="word1">
-                      111111111这里是 简介 这里是简介这里是 简介 这里是简介
-                    这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介                </div>
-
+            <div class="section">
+                    <h2 class="side-title">摘要</h2>
+                    <h2 class="content-title">摘要</h2>
+                    <div class="long-words-box" id="word1">
+                                111111111这里是 简介 这里是简介这里是 简介 这里是简介
+                                这里是 简介 这里是简介这里是 简介 这里是简介这里是
+                                 简介 这里是简介这里是 简介 这里是简介这里是 简
+                                  这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介 
+                    </div>
             </div>
             <div class="section">
                 <h2 class="side-title">功能1 识别</h2>
-                <div class="video-area"></div>
-                <div class="btn-box"> <div class="btn-inner">试一试</div> </div>
-                <!--<div class="slide"><h3>第二屏的第一屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第二屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第三屏</h3></div>-->
+
+                <div class="video-area">
+                <div class="change-speed">
+                    <span>速度调整: </span>
+                    <span><input class="speed" type="radio" name="vehicle" value="0.5" />0.5 </span>
+                    <span><input class="speed" type="radio" name="vehicle" value="1.0" checked="checked" />1.0 </span>
+                    <span><input class="speed" type="radio" name="vehicle" value="1.5" />1.5 </span>
+                    <span><input class="speed" type="radio" name="vehicle" value="2.0" />2.0 </span>
+                </div>
+
+                <div class="real-area">
+                    <video  id="testVideo" controls="controls" width="50%">
+                        <source src="assets/video/test.mp4" type="video/mp4">
+                    </video>
+                </div>
+                </div>
+
+                <div class="btn-box">
+                    <div class="btn-inner">试一试</div>
+                </div>
+                
             </div>
+
             <div class="section">
                 <h2 class="side-title" style="margin-bottom: 10px;width: 90%">功能2 生成</h2>
 
@@ -43,7 +62,7 @@ const dom1=` <div id="dowebok">
 
                 </div>
             </div>
-              </div>`
+        </div>`
 
 
 
