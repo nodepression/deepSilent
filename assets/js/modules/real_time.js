20c1153cad5117e19aef731753ca60b506e178c5
weng <1906338209@qq.com>
2018-05-14 23:27:05 +0800

    drawImg

--- assets/js/modules/real_time.js
+++ assets/js/modules/real_time.js
@@ -42,14 +42,14 @@ function loadImg(index) {
     //     return;
     // }
 }
-function drawImg(url) {
+function drawImg(url) { 
     if (!document.getElementById('ctx')) {
         return;
     }
     var ctx = document.getElementById('ctx').getContext('2d');
     var img = new Image();
     // var str = index.toString().padStart(12, '0');
-
+    
     img.src = url;
     // console.log(url)
     var wid = document.getElementById('pic-v').offsetWidth;
@@ -57,18 +57,29 @@ function drawImg(url) {
     var hei = document.getElementById('pic-v').offsetHeight;
     img.onload = () => {
         // console.log('render')
-        if (document.getElementById('ctx') && start) {
-            ctx.drawImage(img, 0, 0, 300, 150);
-
+        if (document.getElementById('ctx')&&start) {
+            document.getElementById('ctx').width=640;
+            document.getElementById('ctx').height=360;
+            ctx.drawImage(img, 0, 0,640,360);
+           
+            
         } else {
             return;
         }
 
     }
-    img.onerror = () => {
-        return;
+    try {
+
+        img.onerror = function (err) {
+            // setTimeout(function () {
+            //     loadImg(index)
+            // }, 500)
+            throw err
+        }
+    } catch (err) {
+
     }
-}
+ }
 // loadImg(0);
 
 function hasUserMedia() {
@@ -190,7 +201,7 @@ function startF() {
      * 
     */
 
-    loadImg(0);
+    // loadImg(0);
 
     /**
      * socket
@@ -203,7 +214,7 @@ function startF() {
     window.client = io();
     client.on('message', function (msg) {
         // var json = JSON.parse(msg.data);
-        // console.log(msg);
+        console.log(msg);
         chartObj.chart0.data0.push(msg.people[0].pose_keypoints_2d[0]);
         chartObj.chart0.data1.push(msg.people[0].pose_keypoints_2d[1]);
         chartObj.chart0.date.push(new Date(msg.date).toLocaleTimeString());
@@ -211,7 +222,10 @@ function startF() {
         chartObj.chart0 = temp;
 
         //json中需要一个字段指定图片url,绘制图片
-        // drawImg();
+        var path =msg.imgUrl.substr(msg.imgUrl.indexOf('json')+5);
+        var str = 'http://localhost:3000/assets/output/img/'+path;
+        console.log(str)
+        drawImg(str);
 
 
 
